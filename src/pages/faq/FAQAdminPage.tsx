ad92502628e7632c10503e87a47bab9e9f9b4b65
Andrew Simon <dev1and@proton.me>
2026-01-12 17:58:07 +0530

    Removed extra spaces

--- src/pages/faq/FAQAdminPage.tsx
+++ src/pages/faq/FAQAdminPage.tsx
@@ -14,7 +14,6 @@ import {
 import { useToast } from '../../context/ToastContext';
 import Swal from 'sweetalert2';
 
-
 const FAQAdminPage: React.FC = () => {
     const { showToast } = useToast();
     const [faqs, setFaqs] = useState<FAQ[]>([]);
